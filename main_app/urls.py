b9d4b27dd68b6c66508017f3311b9c17683e978f
Sophie Delice <delicesophie824@gmail.com>
2023-04-22 01:13:09 -0400

    wip: adds filesystem

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('books/create/', views.BookCreate.as_view(), name='book_create'), 
     path('books/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'), 
     path('books/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'), 
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
 from .models import Book
+from django.contrib.auth import login 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+
 # from django.http import HttpResponse
 
 # class Book:
@@ -24,20 +29,42 @@ def about(request):
     # return HttpResponse('<h1>About My Book Collector </h1>')
     return render(request, 'about.html')
 
+@login_required
 def books_index(request):
-    books = Book.objects.all()
+    books = Book.objects.filter(user=request.user)
     return render(request, 'books/index.html',{'books':books})
 
+@login_required
 def books_detail(request, book_id):
     book = Book.objects.get( id = book_id)
     return render(request, 'books/detail.html', {'book': book, })
-    
 
-class BookCreate(CreateView):
+
+def signup(request): 
+    error_message =''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('books_index')
+        else: 
+            print(form.erros)
+            error_message = 'Invalid sign up - try again'
+        form = UserCreationForm()
+        return render(request, 'registration/signup.html', {
+            'form': form , 
+            'error': error_message
+         })
+
+class BookCreate(LoginRequiredMixin, CreateView):
     model = Book
     fields = '__all__'
     template_name = 'books/book_form.html'
     success_url ='/books/'
+    def form_valid(self, form): 
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class BookUpdate(UpdateView):
     model = Book 
